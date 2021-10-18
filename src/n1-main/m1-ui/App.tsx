74252e865fac3ecae3536b3ce735346f98cd9d37
BriWS <brightwiths@gmail.com>
2021-10-18 15:02:24 +0300

    pre progress

---
+++ src/n1-main/m1-ui/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            {/*hashRouter, provider*/}
+            <>
+                {/*в хэдер добавим навлинки чтобы прыгать по проекту через хэдер, а не через логику*/}
+                {/*<Main/>*/}
+                    {/* внутри Main отрисовываем Header и роуты */}
+                    {/*<Header/>*/}
+            </>
+        </div>
+    );
+}
+
+export default App;
