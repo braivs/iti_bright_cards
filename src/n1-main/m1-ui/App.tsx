a7f0607859364e33f661442696f7ecac3c8c9d05
BriWS <brightwiths@gmail.com>
2021-10-19 12:10:28 +0300

    +switch and redirect to 404

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import {HashRouter, NavLink} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import './App.css';
-import {Examples} from "./routes/Examples/Examples";
-import {Route} from "react-router-dom";
-import {Login} from "./routes/Login";
-import {Registration} from "./routes/Registration";
-import {Profile} from "./routes/Profile";
-import {Page404} from "./routes/Page404";
-import {PasswordRecovery} from "./routes/PasswordRecovery";
-import {PasswordNew} from "./routes/PasswordNew";
 import {Main} from "./p2-main/Main";
 
 export const App = () => {
@@ -16,14 +8,6 @@ export const App = () => {
         <div className="App">
             <HashRouter />
             <Main />
-            {/*hashRouter, provider*/}
-            <>
-                {/*в хэдер добавим навлинки чтобы прыгать по проекту через хэдер, а не через логику*/}
-                {/*<p2-main/>*/}
-                    {/* внутри p2-main отрисовываем Header и роуты */}
-                    {/*<Header/>*/}
-
-            </>
         </div>
     );
 }
--- src/n1-main/m1-ui/p2-main/Main.tsx
+++ src/n1-main/m1-ui/p2-main/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Header} from "../p1-header/Header";
-import {Route} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import {Login} from "../routes/Login";
 import {Registration} from "../routes/Registration";
 import {Profile} from "../routes/Profile";
@@ -12,14 +12,17 @@ import {Examples} from "../routes/Examples/Examples";
 export const Main = () => {
     return (
         <div>
-            <Header />
-            <Route exact path={'/login'} render={() => <Login/>}/>
-            <Route exact path={'/registration'} render={() => <Registration/>}/>
-            <Route exact path={'/profile'} render={() => <Profile/>}/>
-            <Route exact path={'/404'} render={() => <Page404/>}/>
-            <Route exact path={'/passwordrecovery'} render={() => <PasswordRecovery/>}/>
-            <Route exact path={'/passwordnew'} render={() => <PasswordNew/>}/>
-            <Route exact path={'/tests'} render={() => <Examples/>}/>
+            <Header/>
+            <Switch>
+                <Route exact path={'/login'} render={() => <Login/>}/>
+                <Route exact path={'/registration'} render={() => <Registration/>}/>
+                <Route exact path={'/profile'} render={() => <Profile/>}/>
+                <Route exact path={'/404'} render={() => <Page404/>}/>
+                <Route exact path={'/passwordrecovery'} render={() => <PasswordRecovery/>}/>
+                <Route exact path={'/passwordnew'} render={() => <PasswordNew/>}/>
+                <Route exact path={'/tests'} render={() => <Examples/>}/>
+                <Redirect from={'*'} to={'/404'}/>
+            </Switch>
         </div>
     );
 }
