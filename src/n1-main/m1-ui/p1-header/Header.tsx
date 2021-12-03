c0ed766ffbb1bd9f9a1e8995f82b76abedbee85f
Kravchuk Anton <Kravch-anton@mail.ru>
2021-12-03 19:17:51 +0300

    adding redirect,disabled button and fix header

--- src/n1-main/m1-ui/p1-header/Header.tsx
+++ src/n1-main/m1-ui/p1-header/Header.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import s from './Header.module.scss'
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {LogoutTC} from "../../m2-bll/authReducer";
+import {AppStoreType} from "../../m2-bll/store";
 
 export const Header = () => {
     const dispatch = useDispatch()
     const logout = () => {
         dispatch(LogoutTC())
     }
+    const isLoggedIn = useSelector<AppStoreType, boolean>(state => state.auth.isLoggedIn)
 
     return (
         <div className={s.header}>
-            <NavLink to={'/profile'}><div className={s.logo}>BrightCards</div></NavLink>
-            <div className={s.links}>
+            <NavLink to={'/profile'}>
+                <div className={s.logo}>BrightCards</div>
+            </NavLink>
+            {isLoggedIn ? <div className={s.links}>
                 <NavLink className={s.item} to={'/table'}>Table</NavLink>
+                <NavLink className={s.item} to={'/profile'}>Profile</NavLink>
+                <a className={s.link} onClick={logout}>Log out</a>
+            </div> : <div className={s.links}>
                 <NavLink className={s.item} to={'/login'}>Login</NavLink>
                 <NavLink className={s.item} to={'/registration'}>Registration</NavLink>
-                <NavLink className={s.item} to={'/profile'}>Profile</NavLink>
                 <NavLink className={s.item} to={'/passwordrecovery'}>PasswordRecovery</NavLink>
-                <NavLink className={s.item} to={'/tests'}>Tests</NavLink>
-                <a className={s.link} onClick={logout}>Log out</a>
-            </div>
+            </div>}
 
         </div>
     );
